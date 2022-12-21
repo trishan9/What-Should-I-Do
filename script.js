7045250ff010760b23302035cb8b3a4ec6df6d3f
Trishan Wagle <mailtotrishan@gmail.com>
2022-12-21 14:17:33 +0545

    FEAT - Added deleting activities button

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let fetchBtn = document.querySelector(".activityBtn")
+let deleteBtn
 let activitiesDiv = document.querySelector(".renderActivity")
 const clearBtn = document.querySelector(".clearBtn")
 let loadText = document.querySelector(".load")
-let url = 'https://www.boredapi.com/api/activity'
 let activitiesArr = []
-let typesArr = []
+let url = 'https://www.boredapi.com/api/activity'
 
 
 const fetchActivity = async () => {
+    activitiesArr = []
     loadText.innerHTML = 'Loading activities....'
     let response = await fetch(url)
     let result = await response.json()
@@ -15,24 +16,44 @@ const fetchActivity = async () => {
 
     activitiesArr.push({ 'activity': activity, 'typeOfActivity': type })
 
-    let ihtml = `
-    <p class="activity">${activity}<br/>Type: ${type.charAt(0).toUpperCase() + type.slice(1)}</p>
-    `
+    activitiesArr.forEach((data) => {
+        let ihtml = `
+        <div class="activityDiv">
+        <p class="activity">${data.activity}<br/>Type: ${data.typeOfActivity.charAt(0).toUpperCase() + data.typeOfActivity.slice(1)}</p>
+        <img src="trash.png" alt="" class="delete-icon">
+        </div>
+        `
+        loadText.innerHTML = ''
+        activitiesDiv.innerHTML += ihtml
 
-    loadText.innerHTML = ''
-    activitiesDiv.innerHTML += ihtml
+        deleteBtn = document.querySelectorAll(".delete-icon")
+        deleteBtn.forEach((btn, index, arr) => {
+            btn.addEventListener("mouseenter", () => {
+                btn.src = 'hover-trash.png'
+            })
+
+            btn.addEventListener("mouseleave", () => {
+                btn.src = 'trash.png'
+            })
+
+            btn.addEventListener("click", () => {
+                let element = deleteBtn[index].parentElement
+                element.remove()
+                loadText.innerHTML = 'Activity deleted succesfully!!!'
+                setTimeout(clearLoadFunc, 500)
+            })
+        })
+    })
 }
 
 const clearFunc = () => {
     activitiesDiv.innerHTML = ''
-    loadText.innerHTML = 'Activities cleared succesfully!!!'
-    setTimeout((func) => {
-        loadText.innerHTML = ''
-    }, 500)
+    loadText.innerHTML = 'All activities are cleared succesfully!!!'
+    setTimeout(clearLoadFunc, 500)
 }
 
-const deleteFunc = () => {
-
+const clearLoadFunc = () => {
+    loadText.innerHTML = ''
 }
 
 fetchBtn.addEventListener("click", fetchActivity)
